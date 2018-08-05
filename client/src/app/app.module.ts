7424762a8844242a7f4aa137af8de9882d161ec2
Azeem Mirza <contact@azeemirza.com>
2018-08-06 01:07:52 +0500

    Chat Comp Init

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MessageListComponent } from './message-list/message-list.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MessageListComponent
+    MessageListComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
