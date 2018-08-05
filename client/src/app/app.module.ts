cf7476d0b714261e9c403fa1f7f815e151cfbbaf
Azeem Mirza <contact@azeemirza.com>
2018-08-05 22:33:26 +0500

    Message List Init

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MessageListComponent } from './message-list/message-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MessageListComponent
   ],
   imports: [
     BrowserModule
