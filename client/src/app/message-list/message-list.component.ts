1f86b3cc73f87c8d387698a2d9183256eb001348
Azeem Mirza <contact@azeemirza.com>
2018-08-06 01:00:15 +0500

    Message List Comp view Complete

--- client/src/app/message-list/message-list.component.ts
+++ client/src/app/message-list/message-list.component.ts
@@ -7,9 +7,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageListComponent implements OnInit {
 
+  messages = [
+    { sender : { name: 'Time Berner', picture: 'assets/blank.png'},
+      message : { text : 'This is message, lorep ipsum epsum epsum epsum', time : '2018-07-14 17:29:44' }
+    },
+    { sender : { name: 'Time Berner', picture: 'assets/blank.png'},
+      message : { text : 'This is message, lorep ipsum epsum epsum epsum', time : '2018-07-14 17:29:44' }
+    },
+    { sender : { name: 'Time Berner', picture: 'assets/blank.png'},
+      message : { text : 'This is message, lorep ipsum epsum epsum epsum', time : '2018-07-14 17:29:44' },
+    { sender : { name: 'Time Berner', picture: 'assets/blank.png'},
+      message : { text : 'This is message, lorep ipsum epsum epsum epsum', time : '2018-07-14 17:29:44' }
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
+    s
+  }
+
+  calTime ( datetime) {
+
+
+    //console.log('Provided DTG: ' + new Date(datetime));
+    //console.log('Current DTG: ' + new Date());
+
+    //datetime = typeof datetime !== 'undefined' ? datetime : "2014-01-01 01:02:03.123456";
+    datetime = new Date( datetime ).getTime();
+
+    let now = new Date().getTime();
+
+
+    if( isNaN(datetime) )
+    {
+      return "";
+    }
+
+    //console.log( datetime + " " + now);
+    let milisec_diff;
+    if (datetime < now) {
+      //console.log('check');
+      milisec_diff = now - datetime;
+    }else{
+
+      milisec_diff = datetime - now;
+    }
+
+    let days = Math.floor(milisec_diff / 1000 / 60 / (60 * 24));
+
+    let date_diff = new Date( milisec_diff );
+
+   // console.log(days + " Days "+ date_diff.getHours() + " Hours " + date_diff.getMinutes() + " Minutes " + date_diff.getSeconds() + " Seconds");
+
+    if (days) {
+      return days + ' d';
+    } else if (date_diff.getHours()) {
+      return date_diff.getHours() + ' h';
+    } else if (date_diff.getMinutes()) {
+      return date_diff.getMinutes() + ' m';
+    }else {
+      return 'now';
+    }
+
+
+
   }
 
 }
