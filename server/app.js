6bc6c98f98d14050d067e282e5c613a0ec55df49
Azeem Mirza <contact@azeemirza.com>
2018-08-06 23:43:43 +0500

    app.js update

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express'),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +14,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Socket
+
+
+//Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
