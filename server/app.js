d1f59bce8322d127980d17e99b91c115f340fed8
Azeem Mirza <contact@azeemirza.com>
2018-08-09 00:30:40 +0500

    Mongoose COnnection modulation

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express'),
     path = require('path'),
     cookieParser = require('cookie-parser'),
     logger = require('morgan'),
-    mongoose = require('mongoose');
+    db = require ('./models/index');
 
 var indexRouter = require('./routes/index'),
     usersRouter = require('./routes/users');
@@ -13,16 +13,28 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+/*
 app.use(express.static(path.join(__dirname, 'public')));
+*/
 
 //Socket
 
 //Mongoose
 
-mongoose.connect('mongodb://localhost:27017/chat', {useNewUrlParser: true});
 
 //Routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/*app.use('/', indexRouter);
+app.use('/users', usersRouter);*/
+
+app.use('/', (req, res) => {
+    res.json({
+        title : 'Chat API',
+        url : 'http://localhost:3000/',
+        host : 'localhost',
+        port : '3000'
+    });
+});
+app.db = db;
 
 module.exports = app;
---
+++ server/models/index.js
@@ -0,0 +1,27 @@
+/**
+ * @file index.js
+ * @project chat
+ * @author azeem-mirza
+ *
+ * @date 8/8/2018
+ * @time  10:56 PM
+ *
+ * @copright Azeem Mirza - 2018
+ */
+
+const mongoose = require('mongoose'),
+    uri = 'mongodb://localhost:27017/chat',
+    option = { useNewUrlParser : true };
+
+mongoose.connect(uri, option)
+    .then((res)=>{
+        console.log('Connection Established :');
+        console.log(res);
+    })
+    .catch((err)=>{
+        console.error('Error: ' + err);
+    });
+
+
+
+module.exports = mongoose;
