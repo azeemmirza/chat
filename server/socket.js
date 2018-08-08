269d92bc5a4d1f8765e7e9caa23b7ecd28940db2
Azeem Mirza <contact@azeemirza.com>
2018-08-09 03:14:24 +0500

    Socket Init #2

---
+++ server/socket.js
@@ -0,0 +1,12 @@
+/**
+ * @file socket.js
+ * @project chat
+ * @author azeem-mirza
+ *
+ * @date 8/9/2018
+ * @time  3:12 AM
+ *
+ * @copright Azeem Mirza - 2018
+ */
+
+const socket = require('socket.io');
