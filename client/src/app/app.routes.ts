6c8182c43a354a99406b39dfee91ea0196215990
Azeem Mirza <contact@azeemirza.com>
2018-08-12 22:29:26 +0500

    App Routes Init

---
+++ client/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+/**
+ * @file app.routes.ts
+ * @project chat
+ * @author azeem-mirza
+ *
+ * @date 8/10/2018
+ * @time  2:05 AM
+ *
+ * @copright Azeem Mirza - 2018
+ */
+
+export const AppRoutes = [
+
+];
